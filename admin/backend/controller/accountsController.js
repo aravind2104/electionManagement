2ab24e07a88a8fa47a27e4e00988ff1e911539bc
Chriss Charls <chrisscharls9@gmail.com>
2025-03-07 13:06:58 +0530

    admin backend work done

--- admin/backend/controller/accountsController.js
+++ admin/backend/controller/accountsController.js
@@ -9,40 +9,46 @@ export const uploadStudents = async (req, res) => {
             return res.status(400).json({ error: "No file uploaded" });
         }
 
-        const students = [];
-
         fs.createReadStream(req.file.path)
             .pipe(csv())
             .on("data", async (row) => {
                 try {
                     const { name, rollno, email, password } = row;
-                    console.log(row)
+
                     if (!name || !rollno || !email || !password) {
-                        console.error("Missing field in CSV row:", row);
-                        return; // Skip this row
+                        console.warn("Skipping row due to missing fields:", row)
+                        return;
+                    }
+
+                    const existingStudent = await Student.findOne({ email });
+                    if (existingStudent) {
+                        console.warn(`Skipping duplicate student: ${email}`)
+                        return;
                     }
 
                     const hashedPassword = await bcrypt.hash(password, 10);
 
-                    students.push({
+                    const newStudent = new Student({
                         name,
                         rollno,
                         email,
                         password: hashedPassword,
                     });
+
+                    await newStudent.save();
+                    insertedCount++;
                 } catch (err) {
                     console.error("Error processing row:", err);
                 }
             })
-            .on("end", async () => {
-                try {
-                    await Student.insertMany(students);
-                    res.status(201).json({ message: "Students uploaded successfully" });
-                } catch (err) {
-                    console.error("Error saving students to DB:", err);
-                    res.status(500).json({ error: "Failed to save students" });
-                }
+            .on("end", () => {
+                console.log(`Upload complete`);
+                res.status(201).json({
+                    message: "Student upload completed",
+
+                });
             });
+   
     } catch (error) {
         console.error("Error uploading students:", error);
         res.status(500).json({ error: "Internal server error" });
